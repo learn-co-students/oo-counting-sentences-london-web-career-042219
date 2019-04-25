3ec9543438200fbcc07ed45ceacd4223ce37a260
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-25 18:07:21 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject{ |item| item==""}.length
   end
 end
+
+#binding.pry
+#puts "Mischief Managed!"
