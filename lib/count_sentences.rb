9853c687f52328fa1577dd430f944330c7ead2fa
Adna <adna_duran@hotmail.com>
2019-04-25 14:34:55 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
-require 'pry'
+#require 'pry'
 
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+    return true
   end
+  false
+end
 
   def question?
-
+    if self.end_with?("?")
+    return true
+  end
+  false
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+    return true
+  end
+  false
   end
 
   def count_sentences
-
+     self.split(/[.?!]+/).count
   end
+
+  #binding.pry
 end
