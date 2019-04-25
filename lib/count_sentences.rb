602f3ffa5593c361b239c5411c9301a7d5765710
rossmawd <rossmawd@gmail.com>
2019-04-25 19:38:17 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.split('')[-1] == '.' ? true : false
   end
 
   def question?
-
+    self.split('')[-1] == '?' ? true : false
   end
 
   def exclamation?
-
+    self.split('')[-1] == '!' ? true : false
   end
 
-  def count_sentences
-
+  def count_sentences   # 5 lines?
+    count = self.split(' ').map {|string| string.sentence?}
+    count << self.split(' ').map {|string| string.question?}
+    count << self.split(' ').map {|string| string.exclamation?}
+    count = count.flatten
+    count.select {|bool| bool == true}.length
   end
 end
